ac286826c30b11d154c736ef8b46f109d4402572
kashish <kashishbishnoi23@gmail.com>
2025-01-22 19:14:03 +0530

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
-import { firstImage } from '../../utils'
-import { NorthEastArrow } from '../../utils'
+import { firstImage, checkbg, NorthEastArrow } from '../../utils'
+// import {  } from '../../utils'
+
 function Home() {
   return (
     // <div></div>
     <>
-      <div className='w-full bg-black h-content '>
-        <section className='flex w-full items-center h-[100vh] '>
+      <div className='w-full bg-black h-content max-w-full '>
+        <section className='flex max-w-full items-center h-[100vh] '>
   
           <div className='h-full w-[55%] text-white flex flex-col pt-20 gap-16 items-start '>
-          <div className='text-6xl font-bold text-left w-200 relative  left-[15%] z-20 mix-blend-difference'>
-              Create Your Own <span className='text-grey'>Unique Clothing Designs </span> Today
+          <div className='text-6xl font-bold  w-[120%] relative  left-[15%] z-20 '>
+              Create Your Own <span className='text-white'>Uni</span><span className='text-black'>que</span> 
+              <span className='text-grey'> Clothing  Designs</span>
+             <span className='text-white'>Tod</span><span className='text-black'>ay</span>
             </div>
         
+            <div className='h-[30vh] absolute bottom-0 w-[50vw]' >
+              <img src={checkbg} className='h-[600%] brightness-150 w-full object-cover'></img>
+            </div>
+
             <div className='font-semibold text-md relative w-[80%] left-[15%]'>
               Unleash your creativity and design clothing that fits you perfectly. Our platform provides a personalised experience, allowing you to visualize and adjust your designs before finalizing.
             </div>
@@ -28,14 +35,14 @@ function Home() {
           </div>
         </section>
 
-        <hr className='border border-white border-dotted'></hr>
+        {/* <hr className='border border-white border-dotted'></hr> */}
 
 
         {/* Second Part */}
-        <section>
+        {/* <section>
 
 
-        </section>
+        </section> */}
 
       </div>
 
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,6 @@
 import firstImage from '../src/assets/images/home/first.jpg';
 import NorthEastArrow from '../src/assets/images/home/northEastArrow.svg';
+import checkbg from '../src/assets/images/home/checkbg.svg'
+export {checkbg}
 export {NorthEastArrow}
 export {firstImage}
