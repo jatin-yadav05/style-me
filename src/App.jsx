7844427403797a25edc630914347e0052c08e955
jatin <jatin0700.be23@chitkara.edu.in>
2025-01-22 16:49:54 +0530

    Merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
