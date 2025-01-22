e12c61b5b0a8ca1b725ca2f220df8d1cef50e80b
jatin <jatin0700.be23@chitkara.edu.in>
2025-01-22 16:33:34 +0530

    Maded changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
