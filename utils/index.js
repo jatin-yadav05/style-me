ebd5961037a940b9017e13376008550dc0423106
kashish <kashishbishnoi23@gmail.com>
2025-01-23 16:23:47 +0530

    commit

--- utils/index.js
+++
@@ -1,6 +0,0 @@
-import firstImage from '../src/assets/images/home/first.jpg';
-import NorthEastArrow from '../src/assets/images/home/northEastArrow.svg';
-import checkbg from '../src/assets/images/home/checkbg.svg'
-export {checkbg}
-export {NorthEastArrow}
-export {firstImage}
