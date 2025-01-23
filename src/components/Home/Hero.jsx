9dc212f59c152e083e72be7a0e644ec9d475c653
kashish <kashishbishnoi23@gmail.com>
2025-01-23 16:23:47 +0530

    commit

---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { firstImage, checkbg, NorthEastArrow } from '../../utils/index.js'
+
+const Hero = () => {
+  return (
+    <>
+      <section className='flex max-w-full items-center h-[100vh] '>
+  
+  <div className='h-full w-[55%] text-white flex flex-col pt-20 gap-16 items-start '>
+  <div className='text-6xl font-bold  w-[120%] relative  left-[15%] z-20 '>
+      Create Your Own <span className='text-white'>Uni</span><span className='text-black'>que</span> 
+      <span className='text-grey'> Clothing  Designs</span>
+     <span className='text-white'>Tod</span><span className='text-black'>ay</span>
+    </div>
+
+    <div className='h-[30vh]   absolute bottom-0 w-[50vw] overflow-hidden' >
+      <img src={checkbg} className='h-[600%] brightness-150 w-full object-cover'></img>
+    </div>
+
+    <div className='font-semibold text-md relative w-[80%] left-[15%]'>
+      Unleash your creativity and design clothing that fits you perfectly. Our platform provides a personalised experience, allowing you to visualize and adjust your designs before finalizing.
+    </div>
+
+    <button className='border flex items-center gap-2 border-white px-6 py-4 rounded-full relative left-[15%] font-semibold'>
+      <span>Get Started </span> <img src={NorthEastArrow} className='text-white'></img>
+    </button>
+
+  </div>
+  <div className='h-full w-[45%]'>
+    <img src={firstImage} className='h-[100%] w-[100%] object-cover brightness-75'>
+    </img>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default Hero
---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+import firstImage from '../assets/images/home/first.jpg';
+import NorthEastArrow from '../assets/images/home/northEastArrow.svg';
+import checkbg from '../assets/images/home/checkbg.svg'
+export {checkbg}
+export {NorthEastArrow}
+export {firstImage}
