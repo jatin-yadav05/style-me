357afa142e7c2249e900b9f1ff1e3030055996ac
kashish <kashishbishnoi23@gmail.com>
2025-01-26 11:27:05 +0530

    modification

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 // import {  } from '../../utils'
 import Hero from './Home/Hero'
 import Features from './Home/Features'
+import Steps from './Home/Steps'
+import WhyStyleMe from './Home/WhyStyleMe'
+import Customer from './Home/Customer'
+import StyleNow from './Home/StyleNow'
+import Footer from './Footer'
+
+
 
 function Home() {
     return (
@@ -10,6 +17,7 @@ function Home() {
             <div className='w-full bg-black h-content max-w-full '>
 
 
+                
                 <Hero />
 
                 <Features />
@@ -21,7 +29,7 @@ function Home() {
 
                 <StyleNow/>
 
-                <Footer/>
+                <Footer/> 
 
             </div>
 
--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -6,22 +6,21 @@ const Hero = () => {
     <>
       <section className='flex max-w-full items-center h-[100vh] '>
   
-  <div className='h-full w-[55%] text-white flex flex-col pt-20 gap-16 items-start '>
-  <div className='text-6xl font-bold  w-[120%] relative  left-[15%] z-20 '>
-      Create Your Own <span className='text-white'>Uni</span><span className='text-black'>que</span> 
-      <span className='text-grey'> Clothing  Designs  </span>
-     <span className='text-white'>Tod</span><span className='text-black'>ay</span>
-    </div>
+  <div className='h-full w-[55%] text-white flex flex-col pt-20 gap-16 items-center '>
+  <div className='text-6xl font-bold pl-10  z-20 '>
+      Create Your Own Unique
+      <span className='text-grey'> Clothing  Designs  </span> Today
+  </div>
 
-    <div className='h-[30vh]   absolute bottom-0 w-[50vw] overflow-hidden' >
+    <div className='h-[30vh]  absolute bottom-0 w-[50vw] overflow-hidden' >
       <img src={checkbg} className='h-[600%] brightness-150 w-full object-cover'></img>
     </div>
 
-    <div className='font-semibold text-md relative w-[80%] left-[15%]'>
+    <div className='font-semibold text-md relative w-[80%]'>
       Unleash your creativity and design clothing that fits you perfectly. Our platform provides a personalised experience, allowing you to visualize and adjust your designs before finalizing.
     </div>
 
-    <button className='border flex items-center gap-2 border-white px-6 py-4 rounded-full relative left-[15%] font-semibold'>
+    <button className='border flex items-center gap-2 border-white px-6 py-4 rounded-full  font-semibold'>
       <span>Get Started </span> <img src={NorthEastArrow} className='text-white'></img>
     </button>
 
